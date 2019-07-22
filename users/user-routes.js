32902e86323dd184f13a42e5ab11ac36d0a7f9e9
Nabeelah <nabeelahyousuph@gmail.com>
2019-07-22 15:03:49 +0100

    Create login route

--- users/user-routes.js
+++ users/user-routes.js
@@ -4,7 +4,7 @@ const Users = require("./users-model");
 
 const router = express.Router();
 
-router.post("/register",  async(req, res) => {
+router.post("/register", async (req, res) => {
   try {
     let user = req.body;
     user.password = bcrypt.hashSync(req.body.password, 12);
@@ -12,6 +12,20 @@ router.post("/register",  async(req, res) => {
     res.status(201).json(newUser);
   } catch (error) {
     res.status(500).json("Cannot add user");
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const loggedUser = await Users.findUserBy({ username });
+    if (loggedUser && bcrypt.compareSync(password, loggedUser.password)) {
+      res.status(200).json({ message: `Welcome ${loggedUser.username}!` });
+    } else {
+      res.status(401).json({ message: "Invalid Credentials" });
+    }
+  } catch (error) {
+    res.status(500).json("Cannot login");
   }
 });
 
