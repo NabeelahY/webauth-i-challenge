716e87fcd78cee496953428e656cd9b074ea37b4
Nabeelah <nabeelahyousuph@gmail.com>
2019-07-22 13:08:15 +0100

    Craete method to add new user

---
+++ users/users-model.js
@@ -0,0 +1,14 @@
+const db = require("../database/dbConfig.js");
+
+module.exports = {
+  add
+};
+
+function add(user) {
+  return db("users")
+    .insert(user, "id")
+    .then(ids => {
+      const [id] = ids;
+      return findById(id);
+    });
+}
